ae978fe3e57b5038d7b3d97be638e9a1dd1a14d3
silviolapto <sss.silq@gmail.com>
2023-11-24 17:57:52 -0500

    Estructurando Grid en página principal

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,16 +1,3 @@
-/**
- * Import dependencies from node_modules
- * see commented examples below
- */
-
-// import 'some-node-module';
-// import SomeModule from 'some-node-module';
-
-/**
- * Write any other JavaScript below
- */
-
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+function menu() {
+  document.getElementById('menu').classList.toggle('visible');
+}
