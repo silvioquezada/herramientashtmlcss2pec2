ba638f56666581edeb09929ab27b3604d35ea928
“silvioescritorio” <sss.silq@gmail.com>
2023-11-29 09:18:36 -0500

    Finalizado la agregación de componentes bootstrap

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,3 +1,10 @@
-function menu() {
-  document.getElementById('menu').classList.toggle('visible');
-}
+// function menu() {
+//   document.getElementById('menu').classList.toggle('visible');
+// }
+// Import all plugins
+
+//import "../../../node_modules/bootstrap/js/dist/util.js";
+//import "../../../node_modules/bootstrap/js/dist/modal.js";
+
+import "../../../node_modules/bootstrap/js/dist/modal.js";
+import "../../../node_modules/bootstrap/js/dist/popover.js";
